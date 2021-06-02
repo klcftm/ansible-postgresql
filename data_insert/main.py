faf7a3c1123232e84656e1ef8f1873f090ca2563
Fatma Kılıç <ftmklc1996@gmail.com>
2021-06-02 22:03:33 +0300

    update insert db

--- data_insert/main.py
+++ data_insert/main.py
@@ -2,6 +2,7 @@ import json
 from configparser import ConfigParser
 import psycopg2
 
+
 def config(filename='database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
@@ -20,17 +21,18 @@ def config(filename='database.ini', section='postgresql'):
 
     return db
 
-def create_tables(table_name,fields):
+
+def create_tables(table_name, fields):
     """ create tables in the PostgreSQL database"""
-    size = len(fields)
     columns = []
     for col in fields:
-        columns.append(psycopg2.sql.SQL("{} {}").format(psycopg2.sql.Identifier(col[1]), psycopg2.sql.SQL(col[0])))
+        columns.append(("{} {}").format((col['id']), (col['type'])))
 
-    query = psycopg2.sql.SQL( "CREATE TABLE {tbl_name} ( {fields} );" ).format(
-        tbl_name=psycopg2.sql.Identifier(table_name),
-        fields=psycopg2.sql.SQL(', ').join(columns)
+    query = ("CREATE TABLE {tbl_name} ( {fields} );").format(
+        tbl_name=(table_name),
+        fields=(', ').join(columns)
     )
+    print(query)
     conn = None
     try:
         # read the connection parameters
@@ -50,14 +52,51 @@ def create_tables(table_name,fields):
         if conn is not None:
             conn.close()
 
-def read_json_file_fields(filename):
+
+def insert_tables(table_name, rec):
+    """ insert tables in the PostgreSQL database"""
+    columns = []
+    for count, col in enumerate(rec):
+        if count == 0:
+            columns.append(("{}").format((col)))
+        else:
+            columns.append(("'{}'").format((col)))
+    query = ("INSERT INTO {tbl_name} VALUES( {rec} );").format(
+        tbl_name=(table_name),
+        rec=(', ').join(columns)
+    )
+    print(query)
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(query)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def read_json_file_fields(filename, type):
     # Opening JSON file
     with open(filename, ) as f:
         data = json.load(f)
-        return data['fields']
+        return data[type]
+
 
 if __name__ == '__main__':
-    fields = read_json_file_fields('data/Ocak 2020 Trafik Yo_unluk Verisi.json')
-    create_tables('infradb',fields)
-
-
+    file = 'data/Ocak 2020 Trafik Yo_unluk Verisi.json'
+    fields = read_json_file_fields(file, 'fields')
+    create_tables('infradb2', fields)
+    records = read_json_file_fields(file, 'records')
+    for rec in records:
+        insert_tables('infradb2', rec)
