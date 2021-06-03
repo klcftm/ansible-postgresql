24d65c653ad397fd84fd04947d26b6ebd4b46c51
Fatma Kılıç <ftmklc1996@gmail.com>
2021-06-03 21:57:10 +0300

    Minio insert added

--- data_insert/main.py
+++ data_insert/main.py
@@ -1,6 +1,7 @@
 import json
 from configparser import ConfigParser
 import psycopg2
+from minio import Minio
 
 
 def config(filename='database.ini', section='postgresql'):
@@ -65,7 +66,6 @@ def insert_tables(table_name, rec):
         tbl_name=(table_name),
         rec=(', ').join(columns)
     )
-    print(query)
     conn = None
     try:
         # read the connection parameters
@@ -93,6 +93,24 @@ def read_json_file_fields(filename, type):
         return data[type]
 
 
+def insert_minio(access_key, secret_key, bucket_name, file, file_name):
+    client = Minio(
+        "192.168.1.48:9000",
+        access_key=access_key,
+        secret_key=secret_key,
+        secure=False
+    )
+    """ minio connect"""
+    found = client.bucket_exists(bucket_name)
+    if not found:
+        client.make_bucket(bucket_name)
+    else:
+        print(f"Bucket '{{ bucket_name }}' already exists")
+
+    client.fput_object(bucket_name, file_name, file)
+    print("Successful upload minio")
+
+
 if __name__ == '__main__':
     file = 'data/Ocak 2020 Trafik Yo_unluk Verisi.json'
     fields = read_json_file_fields(file, 'fields')
@@ -100,3 +118,5 @@ if __name__ == '__main__':
     records = read_json_file_fields(file, 'records')
     for rec in records:
         insert_tables('infradb2', rec)
+
+    insert_minio('minio', 'minio', 'minio_bucket', file, 'Ocak 2020 Trafik Yo_unluk Verisi.json')
